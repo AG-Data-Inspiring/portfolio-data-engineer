acaa53aa5a53950256c326f9f38244381cc64dcd
AG-Data-Inpiring <angelag813@outlook.com>
2025-11-18 22:54:28 -0500

    Update package dependencies and configuration files for Tailwind CSS and PostCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", 
+    "./pages/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: 'media', // or 'class' if you want to use class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
